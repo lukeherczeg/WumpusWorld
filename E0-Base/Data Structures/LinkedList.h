60821ebf77f175fa190df37da0b4f433e4eb7584
lukeherczeg <lukeherczeg@ufl.edu>
2020-01-13 00:59:15 -0500

    Added some progress.

--- E0-Base/Data Structures/LinkedList.h
+++ E0-Base/Data Structures/LinkedList.h
@@ -2,5 +2,75 @@
 
 // LinkedList class should go in the "ufl_cap4053::fundamentals" namespace!
 namespace ufl_cap4053 { namespace fundamentals {
+	template <class T>
+	class LinkedList {
 
+		class Iterator {
+		public:
+			T operator*() const {
+				return *T;
+			};
+
+			Iterator& operator++() {
+				return Iterator++;
+			};
+
+			bool operator==(Iterator const& rhs) {
+				if(this->operator==)
+			};
+
+			bool operator!=(Iterator const& rhs) {
+				
+			};
+		};
+
+	public:
+		LinkedList<T>() {
+			
+		};
+
+		Iterator begin() const {
+			
+		};
+
+		Iterator end() const {
+			
+		};
+
+		bool isEmpty() const {
+		
+		};
+
+		T getFront() const {
+		
+		};
+
+		T getBack() const {
+		
+		};
+
+		void enqueue(T element) {
+		
+		};
+
+		void dequeue() {
+		
+		};
+
+		void pop() {
+		
+		};
+
+		void clear() {
+		
+		};
+
+		bool contains(T element) const {
+		
+		};
+
+		void remove(T element) {
+		
+		};
+	};
 } }  // namespace ufl_cap4053::fundamentals
